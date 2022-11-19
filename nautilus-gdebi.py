a08688fa98f0229bddf9764acb9f6f950ef38a16
Tyler Magee Shields <115214762+TylerMS887@users.noreply.github.com>
2022-11-19 21:36:41 +0000

    Update nautilus-gdebi.py

--- nautilus-gdebi.py
+++ nautilus-gdebi.py
@@ -1,33 +1,36 @@
 from gi.repository import Nautilus, GObject, Gio
 import subprocess
 
-SUPPORTED_FORMATS = "application/vnd.debian.binary-package"
+SUPPORTED_FORMATS = 'application/vnd.debian.binary-package'
 
-class GNOMEGDebi(GObject.GObject, Nautilus.MenuProvider):
-   def menu_activate_ch(self, menu, files[0]):
-      if files[0].is_gone():
-        return
-   process = subprocess.run(["gdebi-gtk", files[0]])
-   print("%d" % process.returncode)
-   def get_file_items(self, window, files):
+class GdebiNautilus(GObject.GObject, Nautilus.MenuProvider):
+    def menu_activate_cb(self, menu, file):
+        if file.is_gone():
+            return
+
+        process = subprocess.run(["gdebi-gtk", file.get_location().get_path()])
+        print("%d" % process.returncode)
+
+    def get_file_items(self, window, files):
         if len(files) != 1:
             return
 
-        # We're only going to put ourselves on packages context menus
-        if not files[0].get_mime_type() in SUPPORTED_FORMATS:
+        file = files[0]
+
+        # We're only going to put ourselves on images context menus
+        if not file.get_mime_type() in SUPPORTED_FORMATS:
             return
 
         # Gnome can only handle file:
         # In the future we might want to copy the file locally
-        if files[0].get_uri_scheme() != 'file':
+        if file.get_uri_scheme() != 'file':
             return
 
-        item = Nautilus.MenuItem(name='Nautilus::install_debian',
-                                 label='Install Software',
-                                 tip='Install this software to your system using GDebi')
-        item.connect('activate', self.menu_activate_cb, files[0])
-        return item
+        item = Nautilus.MenuItem(name='Nautilus::install_traditional_package',
+                                 label='Install Debian Package',
+                                 tip='Install the software archived in this package')
+        item.connect('activate', self.menu_activate_cb, file)
+        return item,
 
-   def get_background_items(self, window, files[0]):
+    def get_background_items(self, window, file):
         return None
-
