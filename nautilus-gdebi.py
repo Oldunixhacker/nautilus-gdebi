5bc5fe5eeaccd589c4febef5d8a850feb1371d03
Tyler Magee Shields <115214762+TylerMS887@users.noreply.github.com>
2022-11-19 14:53:44 +0000

    Update nautilus-gdebi.py

--- nautilus-gdebi.py
+++ nautilus-gdebi.py
@@ -8,26 +8,25 @@ class GNOMEGDebi(GObject.GObject, Nautilus.MenuProvider):
       if file.is_gone():
         return
    process = subprocess.run(["gdebi-gtk", file])
+   print("%d" % process.returncode)
    def get_file_items(self, window, files):
         if len(files) != 1:
             return
 
-        file = files[0]
-
-        # We're only going to put ourselves on images context menus
-        if not file.get_mime_type() in SUPPORTED_FORMATS:
+        # We're only going to put ourselves on packages context menus
+        if not files[0].get_mime_type() in SUPPORTED_FORMATS:
             return
 
         # Gnome can only handle file:
         # In the future we might want to copy the file locally
-        if file.get_uri_scheme() != 'file':
+        if files[0].get_uri_scheme() != 'file':
             return
 
         item = Nautilus.MenuItem(name='Nautilus::install_debian',
                                  label='Install Software',
                                  tip='Install this software to your system using GDebi')
         item.connect('activate', self.menu_activate_cb, file)
-        return item,
+        return item
 
    def get_background_items(self, window, file):
         return None
